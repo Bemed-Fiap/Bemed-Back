b1fd0e91196cb30eba9ec83b8548afa1946eb74d
Michel <mich3l.santana0@gmail.com>
2021-03-14 15:21:02 -0300

    #feature - iniciando auth (TokenService)

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,20 @@
 import express from 'express';
+import { TokenService } from './../services/token.service';
+
 const routes = express.Router();
+const _tokenService = new TokenService();
 
 routes.get('/', async (req, res) => {
     return res.send('Hello Bemed');
+});
+
+routes.get('/auth', async (req, res) => {
+    const tokenGenerated = await _tokenService.Generate(['michel', 'mail@mail.com']);
+    const tokenRecovered = await _tokenService.DecifrarToken(tokenGenerated);
+    return res.json({
+        tokenGenerated,
+        tokenRecovered
+    });
 });
 
 export default routes;
