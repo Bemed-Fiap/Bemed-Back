08e7917ab8574d892b0de175a3eaa222f5d74e5f
Michel <mich3l.santana0@gmail.com>
2021-03-14 12:47:15 -0300

    #feature implementando mongodb - classe de conexão

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,22 @@
 import express from 'express';
+import { MongoConnection } from './../database/connection';
+
 const routes = express.Router();
 
 routes.get('/', async (req, res) => {
     return res.send('Hello Bemed');
+});
+
+routes.get('/mongo', async (req, res) => {
+    const _usuario = new MongoConnection('usuario');
+    await _usuario.Connect();
+    const usuarioInserido = await _usuario.Insert({ nome: 'michel 2', sobrenome: ':)' });
+    const todos = await _usuario.All();
+    await _usuario.Close();
+    return res.json({ 
+        usuarioInserido,
+        todos
+    });
 });
 
 export default routes;
